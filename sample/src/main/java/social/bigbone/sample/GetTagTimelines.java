429a02134dc1bd8908edac6d7f07e57461c20438
André Gasser <andre.gasser@protonmail.com>
2022-12-23 11:47:28 +0100

    Enabled PMD (#46)

--- sample/src/main/java/social/bigbone/sample/GetTagTimelines.java
+++ sample/src/main/java/social/bigbone/sample/GetTagTimelines.java
@@ -9,13 +9,14 @@ import social.bigbone.api.entity.Status;
 import social.bigbone.api.method.Public;
 import social.bigbone.api.exception.BigboneRequestException;
 
+@SuppressWarnings({"PMD.SystemPrintln", "PMD.AvoidPrintStackTrace"})
 public class GetTagTimelines {
-    public static void main(String[] args) {
-        MastodonClient client = new MastodonClient.Builder("mstdn.jp", new OkHttpClient.Builder(), new Gson()).build();
-        Public publicMethod = new Public(client);
+    public static void main(final String[] args) {
+        final MastodonClient client = new MastodonClient.Builder("mstdn.jp", new OkHttpClient.Builder(), new Gson()).build();
+        final Public publicMethod = new Public(client);
 
         try {
-            Pageable<Status> statuses = publicMethod.getFederatedTag("mastodon", new Range()).execute();
+            final Pageable<Status> statuses = publicMethod.getFederatedTag("mastodon", new Range()).execute();
             statuses.getPart().forEach(status -> {
                 System.out.println("=============");
                 System.out.println(status.getAccount().getDisplayName());
