2a58150378752c8fdb872ea44da93d0534553f62
bocops <andreas@bocops.de>
2023-01-30 17:29:25 +0100

    remove method to get reports; allow multiple IDs when filing
    According to https://docs.joinmastodon.org/methods/reports/, a method to get previously filed reports does not exist and needs to be removed. Filing a report allows more than one status ID to be attached, whereas we previously supported just one status ID.
    
    Fixing both issues and related tests, closing #117.

--- bigbone/src/main/kotlin/social/bigbone/api/method/ReportMethods.kt
+++ bigbone/src/main/kotlin/social/bigbone/api/method/ReportMethods.kt
@@ -3,8 +3,6 @@ package social.bigbone.api.method
 import social.bigbone.MastodonClient
 import social.bigbone.MastodonRequest
 import social.bigbone.Parameters
-import social.bigbone.api.Pageable
-import social.bigbone.api.Range
 import social.bigbone.api.entity.Report
 import social.bigbone.api.exception.BigBoneRequestException
 
@@ -13,32 +11,21 @@ import social.bigbone.api.exception.BigBoneRequestException
  * @see <a href="https://docs.joinmastodon.org/methods/reports/">Mastodon reports API methods</a>
  */
 class ReportMethods(private val client: MastodonClient) {
-    // GET /api/v1/reports
-    @JvmOverloads
-    @Throws(BigBoneRequestException::class)
-    fun getReports(range: Range = Range()): MastodonRequest<Pageable<Report>> {
-        return client.getPageableMastodonRequest(
-            endpoint = "api/v1/reports",
-            method = MastodonClient.Method.GET,
-            parameters = range.toParameters()
-        )
-    }
-
     /**
      * File a report.
      * @param accountId The ID of the account to report
-     * @param statusId The ID of a status to report
+     * @param statusIds List of ID strings for statuses to be reported
      * @param comment The reason for the report. Default maximum of 1000 characters.
      * @see <a href="https://docs.joinmastodon.org/methods/reports/#post">Mastodon API documentation: methods/reports/#post</a>
      */
     @Throws(BigBoneRequestException::class)
-    fun fileReport(accountId: String, statusId: String, comment: String): MastodonRequest<Report> {
+    fun fileReport(accountId: String, statusIds: List<String>, comment: String): MastodonRequest<Report> {
         return client.getMastodonRequest(
             endpoint = "api/v1/reports",
             method = MastodonClient.Method.POST,
             parameters = Parameters().apply {
                 append("account_id", accountId)
-                append("status_ids", statusId)
+                append("status_ids", statusIds)
                 append("comment", comment)
             }
         )
