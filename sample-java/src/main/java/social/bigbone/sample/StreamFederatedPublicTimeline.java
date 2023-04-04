26e5bd67ae2795161b9fceeb0c1d4f190e64ea5d
Tomohide Fujikawa <lamrongol@gmail.com>
2023-04-04 15:54:51 +0900

    Fix variable names to camelcase because they are only "final" but not "static final" imuutable fields

--- sample-java/src/main/java/social/bigbone/sample/StreamFederatedPublicTimeline.java
+++ sample-java/src/main/java/social/bigbone/sample/StreamFederatedPublicTimeline.java
@@ -11,12 +11,12 @@ import social.bigbone.api.exception.BigBoneRequestException;
 @SuppressWarnings("PMD.SystemPrintln")
 public class StreamFederatedPublicTimeline {
     public static void main(final String[] args) throws BigBoneRequestException, InterruptedException {
-        final String INSTANCE = args[0];
-        final String ACCESS_TOKEN = args[1];
+        final String instance = args[0];
+        final String accessToken = args[1];
 
         // Instantiate client
-        final MastodonClient client = new MastodonClient.Builder(INSTANCE)
-                .accessToken(ACCESS_TOKEN)
+        final MastodonClient client = new MastodonClient.Builder(instance)
+                .accessToken(accessToken)
                 .useStreamingApi()
                 .build();
 
