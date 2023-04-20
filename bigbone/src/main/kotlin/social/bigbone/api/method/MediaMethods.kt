6378ee1dafdfd293aab44a8b0bd68cb2591cc83e
Andreas <andreas@bocops.de>
2023-04-20 13:07:21 +0200

    allow adding a description when uploading media (#191)

--- bigbone/src/main/kotlin/social/bigbone/api/method/MediaMethods.kt
+++ bigbone/src/main/kotlin/social/bigbone/api/method/MediaMethods.kt
@@ -18,18 +18,22 @@ class MediaMethods(private val client: MastodonClient) {
      * Creates an attachment to be used with a new status. This method will return after the full sized media is done processing.
      * @param file the file that should be uploaded
      * @param mediaType media type of the file as a string, e.g. "image/png"
+     * @param description a plain-text description of the media, for accessibility purposes.
      * @param focus a [Focus] instance which specifies the x- and y- coordinate of the focal point. Valid range for x and y is -1.0 to 1.0.
      * @see <a href="https://docs.joinmastodon.org/methods/media/#v1">Mastodon API documentation: methods/media/#v1</a>
      */
     @JvmOverloads
-    fun uploadMedia(file: File, mediaType: String, focus: Focus? = null): MastodonRequest<MediaAttachment> {
+    fun uploadMedia(file: File, mediaType: String, description: String? = null, focus: Focus? = null): MastodonRequest<MediaAttachment> {
         val body = file.asRequestBody(mediaType.toMediaTypeOrNull())
         val part = MultipartBody.Part.createFormData("file", file.name, body)
         val requestBodyBuilder = MultipartBody.Builder()
             .setType(MultipartBody.FORM)
             .addPart(part)
-        if (focus != null) {
-            requestBodyBuilder.addFormDataPart("focus", focus.toString())
+        description?.let {
+            requestBodyBuilder.addFormDataPart("description", it)
+        }
+        focus?.let {
+            requestBodyBuilder.addFormDataPart("focus", it.toString())
         }
         val requestBody = requestBodyBuilder.build()
         return MastodonRequest<MediaAttachment>(
