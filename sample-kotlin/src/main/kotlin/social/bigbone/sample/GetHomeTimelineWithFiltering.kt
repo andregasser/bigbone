c29a891adca9123b4173174718ddf56f3eadf4d8
Andreas <andreas@bocops.de>
2023-06-05 10:45:39 +0200

    sample to showcase potential filtering in home timeline (#221)

---
+++ sample-kotlin/src/main/kotlin/social/bigbone/sample/GetHomeTimelineWithFiltering.kt
@@ -0,0 +1,55 @@
+package social.bigbone.sample
+
+import social.bigbone.MastodonClient
+import social.bigbone.api.entity.Filter
+
+object GetHomeTimelineWithFiltering {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val instance = args[0]
+        val accessToken = args[1]
+
+        // Instantiate client
+        val client = MastodonClient.Builder(instance)
+            .accessToken(accessToken)
+            .build()
+
+        // Get statuses from home timeline, then handle each one
+        val statuses = client.timelines.getHomeTimeline().execute()
+        statuses.part.forEach { status ->
+
+            // determine if we need to hide the status or just warn about it
+            var shouldWarn = false
+            var shouldHide = false
+            status.filtered?.let { filterResultList ->
+                filterResultList.forEach {
+                    val filter = it.filter
+
+                    // only use a filter if it applies to our current context (in this case, the home timeline)
+                    if (filter.context.contains(Filter.Context.Home.value)) {
+
+                        // check the filter action set for this filter
+                        when (filter.filterAction) {
+                            Filter.Action.Warn.value -> shouldWarn = true
+                            Filter.Action.Hide.value -> shouldHide = true
+                        }
+                    }
+                }
+            }
+
+            // display the status as is appropriate
+            if (!shouldHide) {
+                // hidden statuses are skipped completely
+
+                // display warning, or post content if no filters matched
+                if (shouldWarn) {
+                    println("********************************************************************************")
+                    println("* WARNING * status by ${status.account?.displayName} matches one or more of your filters")
+                    println("********************************************************************************")
+                } else {
+                    println("${status.account?.displayName} posted: ${status.content.take(50)}")
+                }
+            }
+        }
+    }
+}
