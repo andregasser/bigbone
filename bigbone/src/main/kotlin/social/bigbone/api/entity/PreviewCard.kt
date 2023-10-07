fd2b59647e514a867e58ba06d9dbc6a57904b6a4
gioxi <desiderio.gio@yahoo.com>
2023-10-07 17:22:42 +0200

    update PreviewCard and Tag dto by also separating shared History class

--- bigbone/src/main/kotlin/social/bigbone/api/entity/PreviewCard.kt
+++ bigbone/src/main/kotlin/social/bigbone/api/entity/PreviewCard.kt
@@ -26,8 +26,87 @@ data class PreviewCard(
     val description: String = "",
 
     /**
+     * The type of the preview card.
+     * @see CardType
+     */
+    @SerializedName("type")
+    val type: String = CardType.Link.value,
+
+    /**
+     * The author of the original resource.
+     */
+    @SerializedName("author_name")
+    val authorName: String = "",
+
+    /**
+     * A link to the author of the original resource.
+     */
+    @SerializedName("author_url")
+    val authorUrl: String = "",
+
+    /**
+     * The provider of the original resource.
+     */
+    @SerializedName("provider_name")
+    val providerName: String = "",
+
+    /**
+     * A link to the provider of the original resource.
+     */
+    @SerializedName("provider_url")
+    val providerUrl: String = "",
+
+    /**
+     * HTML to be used for generating the preview card.
+     */
+    @SerializedName("html")
+    val html: String = "",
+
+    /**
+     * Width of preview, in pixels.
+     */
+    @SerializedName("width")
+    val width: Int = 0,
+
+    /**
+     * Height of preview, in pixels.
+     */
+    @SerializedName("height")
+    val height: Int = 0,
+
+    /**
      * Preview thumbnail.
      */
     @SerializedName("image")
-    val image: String? = null
-)
+    val image: String? = null,
+
+    /**
+     * Used for photo embeds, instead of custom html.
+     */
+    @SerializedName("method_url")
+    val methodUrl: String = "",
+
+    /**
+     * A hash computed by the BlurHash algorithm,
+     * for generating colorful preview thumbnails
+     * when media has not been downloaded yet.
+     */
+    @SerializedName("blurhash")
+    val blurhash: String? = null,
+
+    /**
+     * Usage statistics for given days (typically the past week).
+     */
+    @SerializedName("history")
+    val history: List<History> = emptyList(),
+) {
+    /**
+     * Specifies the type of the preview card.
+     */
+    enum class CardType(val value: String) {
+        Link("link"),
+        Photo("photo"),
+        Video("video"),
+        Rich("iframe")
+    }
+}
--- bigbone/src/main/kotlin/social/bigbone/api/entity/Tag.kt
+++ bigbone/src/main/kotlin/social/bigbone/api/entity/Tag.kt
@@ -30,29 +30,4 @@ data class Tag(
      */
     @SerializedName("following")
     val following: Boolean? = null
-) {
-    /**
-     * Usage statistics for given days (typically the past week).
-     * @see <a href="https://docs.joinmastodon.org/entities/Tag/#history">Mastodon API Tag history</a>
-     */
-    data class History(
-
-        /**
-         * UNIX timestamp on midnight of the given day.
-         */
-        @SerializedName("day")
-        val day: String = "",
-
-        /**
-         * The counted usage of the tag within that day, string cast from integer.
-         */
-        @SerializedName("uses")
-        val uses: String = "",
-
-        /**
-         * The total of accounts using the tag within that day, string cast from integer.
-         */
-        @SerializedName("accounts")
-        val accounts: String = ""
-    )
-}
+)
