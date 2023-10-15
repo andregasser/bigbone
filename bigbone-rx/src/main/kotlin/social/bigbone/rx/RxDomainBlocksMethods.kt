f93d6570c09c610a83a7c7741d8e3bfc0395d0c5
PattaFeuFeu <code@patrick-geselbracht.eu>
2023-10-15 14:56:24 +0200

    Add domain_blocks API

---
+++ bigbone-rx/src/main/kotlin/social/bigbone/rx/RxDomainBlocksMethods.kt
@@ -0,0 +1,46 @@
+package social.bigbone.rx
+
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+import social.bigbone.MastodonClient
+import social.bigbone.api.Pageable
+import social.bigbone.api.Range
+import social.bigbone.api.method.DomainBlocksMethods
+
+/**
+ * Reactive implementation of [DomainBlocksMethods]
+ * Manage a user's blocked domains.
+ * @see <a href="https://docs.joinmastodon.org/methods/domain_blocks/">Mastodon domain_blocks API methods</a>
+ */
+class RxDomainBlocksMethods(client: MastodonClient) {
+
+    private val domainBlocksMethods = DomainBlocksMethods(client)
+
+    /**
+     * View domains the user has blocked.
+     * @param range optional Range for the pageable return value
+     * @see <a href="https://docs.joinmastodon.org/methods/domain_blocks/#get">Mastodon API documentation: methods/domain_blocks/#get</a>
+     * @return
+     */
+    fun getDomainBlocks(range: Range = Range()): Single<Pageable<String>> =
+        Single.fromCallable { domainBlocksMethods.getDomainBlocks(range).execute() }
+
+    /**
+     * Block a domain to hide all public posts from it, hide all notifications from it,
+     * remove all followers from it, prevent following new users from it (but does not remove existing follows).
+     *
+     * @param domain Domain to block
+     * @see <a href="https://docs.joinmastodon.org/methods/domain_blocks/#block">Mastodon API documentation: methods/domain_blocks/#block</a>
+     */
+    fun blockDomain(domain: String): Completable = Completable.fromAction { domainBlocksMethods.blockDomain(domain) }
+
+    /**
+     * Remove a domain block, if it exists in the user’s array of blocked domains.
+     *
+     * @param domain Domain to block
+     * @see <a href="https://docs.joinmastodon.org/methods/domain_blocks/#block">Mastodon API documentation: methods/domain_blocks/#block</a>
+     */
+    fun unblockDomain(domain: String): Completable =
+        Completable.fromAction { domainBlocksMethods.unblockDomain(domain) }
+
+}
---
+++ bigbone/src/test/kotlin/social/bigbone/api/method/DomainBlocksMethodsTest.kt
@@ -0,0 +1,155 @@
+package social.bigbone.api.method
+
+import io.mockk.spyk
+import io.mockk.verify
+import org.amshove.kluent.invoking
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldNotThrow
+import org.amshove.kluent.shouldThrow
+import org.amshove.kluent.withMessage
+import org.junit.jupiter.api.Test
+import social.bigbone.MastodonClient
+import social.bigbone.Parameters
+import social.bigbone.api.Pageable
+import social.bigbone.api.Range
+import social.bigbone.api.exception.BigBoneRequestException
+import social.bigbone.testtool.MockClient
+
+class DomainBlocksMethodsTest {
+
+    @Test
+    fun `Given a client returning success, when getting blocked domains, then expect values of response`() {
+        val client = MockClient.mock("domain_blocks_view_success.json")
+
+        val domainBlocksMethods = DomainBlocksMethods(client)
+        val blockedDomains: Pageable<String> = domainBlocksMethods.getDomainBlocks().execute()
+
+        blockedDomains.part.size shouldBeEqualTo 2
+        blockedDomains.part[0] shouldBeEqualTo "nsfw.social"
+        blockedDomains.part[1] shouldBeEqualTo "artalley.social"
+
+        verify {
+            client.get(
+                path = "/api/v1/domain_blocks",
+                query = any<Parameters>()
+            )
+        }
+    }
+
+    @Test
+    fun `Given a client returning success, when getting blocked domains with a limit of 210, then throw IllegalArgumentException`() {
+        val client = MockClient.mock("domain_blocks_view_success.json")
+
+        invoking {
+            DomainBlocksMethods(client).getDomainBlocks(
+                range = Range(limit = 210)
+            ).execute()
+        } shouldThrow IllegalArgumentException::class withMessage "limit defined in Range must not be higher than 200 but was 210"
+    }
+
+    @Test
+    fun `Given a client returning unauthorized, when getting blocked domains, then propagate error`() {
+        val client = MockClient.failWithResponse(
+            responseJsonAssetPath = "error_401_unauthorized.json",
+            responseCode = 401,
+            message = "Unauthorized"
+        )
+
+        invoking {
+            DomainBlocksMethods(client).getDomainBlocks().execute()
+        } shouldThrow BigBoneRequestException::class withMessage "Unauthorized"
+    }
+
+    @Test
+    fun `Given a client returning success, when blocking a valid domain, then expect no errors`() {
+        val client = MockClient.mock("domain_blocks_block_success.json")
+
+        val domainBlocksMethods = DomainBlocksMethods(client)
+        invoking { domainBlocksMethods.blockDomain("nsfw.social") } shouldNotThrow BigBoneRequestException::class
+
+        verify {
+            client.performAction(
+                endpoint = "/api/v1/domain_blocks",
+                method = MastodonClient.Method.POST
+            )
+        }
+    }
+
+    @Test
+    fun `Given a client returning success, when blocking an invalid domain, then throw IllegalArgumentException`() {
+        val client = MockClient.mock("domain_blocks_block_success.json")
+
+        val domainBlocksMethods = DomainBlocksMethods(client)
+        invoking {
+            domainBlocksMethods.blockDomain("")
+        } shouldThrow IllegalArgumentException::class withMessage "domain must not be blank"
+
+        invoking {
+            domainBlocksMethods.blockDomain("    ")
+        } shouldThrow IllegalArgumentException::class withMessage "domain must not be blank"
+
+        invoking {
+            domainBlocksMethods.blockDomain("example . com")
+        } shouldThrow IllegalArgumentException::class withMessage "domain must not contain spaces"
+    }
+
+    @Test
+    fun `Given a client returning unprocessable entity, when blocking a domain, then propagate error`() {
+        val client = MockClient.failWithResponse(
+            responseJsonAssetPath = "domain_blocks_block_error_422.json",
+            responseCode = 422,
+            message = "Unprocessable entity"
+        )
+
+        invoking {
+            DomainBlocksMethods(client).blockDomain("nsfw.social")
+        } shouldThrow BigBoneRequestException::class withMessage "Unprocessable entity"
+    }
+
+    @Test
+    fun `Given a client returning success, when unblocking a valid domain, then expect no errors`() {
+        val client = spyk(MockClient.mock("domain_blocks_unblock_success.json"))
+
+        val domainBlocksMethods = DomainBlocksMethods(client)
+        invoking { domainBlocksMethods.unblockDomain("nsfw.social") } shouldNotThrow BigBoneRequestException::class
+
+        verify {
+            client.performAction(
+                endpoint = "/api/v1/domain_blocks",
+                method = MastodonClient.Method.DELETE
+            )
+        }
+    }
+
+    @Test
+    fun `Given a client returning success, when unblocking an invalid domain, then throw IllegalArgumentException`() {
+        val client = MockClient.mock("domain_blocks_unblock_success.json")
+
+        val domainBlocksMethods = DomainBlocksMethods(client)
+        invoking {
+            domainBlocksMethods.unblockDomain("")
+        } shouldThrow IllegalArgumentException::class withMessage "domain must not be blank"
+
+        invoking {
+            domainBlocksMethods.unblockDomain("    ")
+        } shouldThrow IllegalArgumentException::class withMessage "domain must not be blank"
+
+        invoking {
+            domainBlocksMethods.unblockDomain("example . com")
+        } shouldThrow IllegalArgumentException::class withMessage "domain must not contain spaces"
+    }
+
+    @Test
+    fun `Given a client returning unprocessable entity, when unblocking a domain, then propagate error`() {
+        val client = MockClient.failWithResponse(
+            responseJsonAssetPath = "domain_blocks_unblock_error_422.json",
+            responseCode = 422,
+            message = "Unprocessable entity"
+        )
+
+        invoking {
+            DomainBlocksMethods(client).unblockDomain("nsfw.social")
+        } shouldThrow BigBoneRequestException::class withMessage "Unprocessable entity"
+    }
+
+}
