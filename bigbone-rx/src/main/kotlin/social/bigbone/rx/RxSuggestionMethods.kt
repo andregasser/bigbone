6c192063e4c7fda1e3730b8d1388d75fb7e4c59c
gioxi <desiderio.gio@yahoo.com>
2023-10-18 23:47:01 +0200

    fix styling

--- bigbone-rx/src/main/kotlin/social/bigbone/rx/RxSuggestionMethods.kt
+++ bigbone-rx/src/main/kotlin/social/bigbone/rx/RxSuggestionMethods.kt
@@ -7,7 +7,6 @@ import social.bigbone.api.method.SuggestionMethods
 import io.reactivex.rxjava3.core.Completable
 import social.bigbone.rx.extensions.onErrorIfNotDisposed
 
-
 /**
  * Reactive implementation of [SuggestionMethods].
  * Allows access to API methods with endpoints having an "api/vX/suggestions" prefix.
