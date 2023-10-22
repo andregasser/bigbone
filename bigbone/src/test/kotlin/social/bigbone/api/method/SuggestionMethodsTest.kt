00fd6639a574f8aea200556f17b9cee532055a23
gioxi <desiderio.gio@yahoo.com>
2023-10-22 22:27:31 +0200

    add suggestions test

--- bigbone/src/test/kotlin/social/bigbone/api/method/SuggestionMethodsTest.kt
+++ bigbone/src/test/kotlin/social/bigbone/api/method/SuggestionMethodsTest.kt
@@ -1,6 +1,7 @@
 package social.bigbone.api.method
 
 import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldHaveSize
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import social.bigbone.api.exception.BigBoneRequestException
@@ -13,9 +14,9 @@ class SuggestionMethodsTest {
         val client = MockClient.mock("suggestions.json")
         val suggestionMethods = SuggestionMethods(client)
         val suggestions = suggestionMethods.getSuggestions(2).execute()
-        suggestions.size shouldBeEqualTo 2
-        suggestions[0].source shouldBeEqualTo "past_interactions"
-        suggestions[1].source shouldBeEqualTo "global"
+        suggestions shouldHaveSize 2
+        suggestions[0].source.name.lowercase() shouldBeEqualTo "past_interactions"
+        suggestions[1].source.name.lowercase() shouldBeEqualTo "global"
     }
 
     @Test
@@ -24,6 +25,15 @@ class SuggestionMethodsTest {
             val client = MockClient.ioException()
             val suggestionMethods = SuggestionMethods(client)
             suggestionMethods.removeSuggestion("accountId")
+        }
+    }
+
+    @Test
+    fun overRangedLimitValueThrowsException() {
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            val client = MockClient.ioException()
+            val suggestionMethods = SuggestionMethods(client)
+            suggestionMethods.getSuggestions(100).execute()
         }
     }
 }
