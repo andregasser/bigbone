fbd81229896c423a67fb0d93ccf348f54c506d6f
PattaFeuFeu <code@patrick-geselbracht.eu>
2023-10-28 23:39:09 +0200

    Add old getInstance method back in as deprecated getInstanceV1

--- bigbone-rx/src/main/kotlin/social/bigbone/rx/RxInstanceMethods.kt
+++ bigbone-rx/src/main/kotlin/social/bigbone/rx/RxInstanceMethods.kt
@@ -6,6 +6,7 @@ import social.bigbone.api.entity.DomainBlock
 import social.bigbone.api.entity.ExtendedDescription
 import social.bigbone.api.entity.Instance
 import social.bigbone.api.entity.InstanceActivity
+import social.bigbone.api.entity.InstanceV1
 import social.bigbone.api.entity.Rule
 import social.bigbone.api.method.InstanceMethods
 
@@ -23,6 +24,17 @@ class RxInstanceMethods(client: MastodonClient) {
      * @see <a href="https://docs.joinmastodon.org/methods/instance/#v2">Mastodon API documentation: methods/instance/#v2</a>
      */
     fun getInstance(): Single<Instance> = Single.fromCallable { instanceMethods.getInstance().execute() }
+
+    /**
+     * Obtain general information about the server.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#v1">Mastodon API documentation: methods/instance/#v1</a>
+     */
+    @Deprecated(
+        message = "Deprecated since 4.0.0. This method is just kept for backward compatibility.",
+        replaceWith = ReplaceWith("getInstance()"),
+        level = DeprecationLevel.WARNING
+    )
+    fun getInstanceV1(): Single<InstanceV1> = Single.fromCallable { instanceMethods.getInstanceV1().execute() }
 
     /**
      * Get the list of connected domains: Domains that this instance is aware of.
