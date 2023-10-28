7b5d053a5b6c920c27c815dd007cb2e7f0e2f038
gioxi <desiderio.gio@yahoo.com>
2023-10-28 13:57:23 +0200

    add data classes for PushMethods

---
+++ bigbone/src/main/kotlin/social/bigbone/api/method/PushMethods.kt
@@ -0,0 +1,12 @@
+package social.bigbone.api.method
+
+import social.bigbone.MastodonClient
+
+/**
+ * Allows access to API methods with endpoints having an "api/vX/push" prefix.
+ * @see <a href="https://docs.joinmastodon.org/methods/push/">Mastodon push API methods</a>
+ */
+class PushMethods(private val client: MastodonClient) {
+
+
+}
