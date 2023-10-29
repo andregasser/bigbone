b42c0605bfbff4c8d173eeb2f6d28f2da6fdf8e9
Patrick Geselbracht <code@patrick-geselbracht.eu>
2023-10-29 11:02:16 +0100

    Implement instance API methods (#303)
    * Replace deprecated v1 getInstance method with v2 version
    
    * Add missing API methods and entities
    
    * Add old getInstance method back in as deprecated getInstanceV1
    
    ---------
    
    Co-authored-by: André Gasser <andre.gasser@protonmail.com>

--- bigbone-rx/src/main/kotlin/social/bigbone/rx/RxInstanceMethods.kt
+++ bigbone-rx/src/main/kotlin/social/bigbone/rx/RxInstanceMethods.kt
@@ -2,7 +2,12 @@ package social.bigbone.rx
 
 import io.reactivex.rxjava3.core.Single
 import social.bigbone.MastodonClient
+import social.bigbone.api.entity.DomainBlock
+import social.bigbone.api.entity.ExtendedDescription
 import social.bigbone.api.entity.Instance
+import social.bigbone.api.entity.InstanceActivity
+import social.bigbone.api.entity.InstanceV1
+import social.bigbone.api.entity.Rule
 import social.bigbone.api.method.InstanceMethods
 
 /**
@@ -14,5 +19,62 @@ class RxInstanceMethods(client: MastodonClient) {
 
     private val instanceMethods = InstanceMethods(client)
 
+    /**
+     * Obtain general information about the server.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#v2">Mastodon API documentation: methods/instance/#v2</a>
+     */
     fun getInstance(): Single<Instance> = Single.fromCallable { instanceMethods.getInstance().execute() }
+
+    /**
+     * Obtain general information about the server.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#v1">Mastodon API documentation: methods/instance/#v1</a>
+     */
+    @Deprecated(
+        message = "Deprecated since 4.0.0. This method is just kept for backward compatibility.",
+        replaceWith = ReplaceWith("getInstance()"),
+        level = DeprecationLevel.WARNING
+    )
+    fun getInstanceV1(): Single<InstanceV1> = Single.fromCallable { instanceMethods.getInstanceV1().execute() }
+
+    /**
+     * Get the list of connected domains: Domains that this instance is aware of.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#peers">Mastodon API documentation: methods/instance/#peers</a>
+     */
+    fun getPeers(): Single<List<String>> = Single.fromCallable {
+        instanceMethods.getPeers().execute()
+    }
+
+    /**
+     * Instance activity over the last 3 months, binned weekly.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#activity">Mastodon API documentation: methods/instance/#activity</a>
+     */
+    fun getActivity(): Single<List<InstanceActivity>> = Single.fromCallable {
+        instanceMethods.getActivity().execute()
+    }
+
+    /**
+     * Rules that the users of this service should follow.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#rules">Mastodon API documentation: methods/instance/#rules</a>
+     */
+    fun getRules(): Single<List<Rule>> = Single.fromCallable {
+        instanceMethods.getRules().execute()
+    }
+
+    /**
+     * View moderated servers
+     * Obtain a list of domains that have been blocked.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#domain_blocks">Mastodon API documentation: methods/instance/#domain_blocks</a>
+     */
+    fun getBlockedDomains(): Single<List<DomainBlock>> = Single.fromCallable {
+        instanceMethods.getBlockedDomains().execute()
+    }
+
+    /**
+     * Obtain an extended description of this server.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#extended_description">
+     *     Mastodon API documentation: methods/instance/#extended_description</a>
+     */
+    fun getExtendedDescription(): Single<ExtendedDescription> = Single.fromCallable {
+        instanceMethods.getExtendedDescription().execute()
+    }
 }
--- bigbone/src/main/kotlin/social/bigbone/api/method/InstanceMethods.kt
+++ bigbone/src/main/kotlin/social/bigbone/api/method/InstanceMethods.kt
@@ -2,20 +2,103 @@ package social.bigbone.api.method
 
 import social.bigbone.MastodonClient
 import social.bigbone.MastodonRequest
+import social.bigbone.api.entity.DomainBlock
+import social.bigbone.api.entity.ExtendedDescription
 import social.bigbone.api.entity.Instance
+import social.bigbone.api.entity.InstanceActivity
+import social.bigbone.api.entity.InstanceV1
+import social.bigbone.api.entity.Rule
 
 /**
  * Allows access to API methods with endpoints having an "api/vX/instance" prefix.
+ * Discover information about a Mastodon website.
  * @see <a href="https://docs.joinmastodon.org/methods/instance/">Mastodon instance API methods</a>
  */
 class InstanceMethods(private val client: MastodonClient) {
+
+    private val instanceEndpointV1 = "/api/v1/instance"
+    private val instanceEndpointV2 = "/api/v2/instance"
+
     /**
-     * Retrieve instance details.
-     * @see <a href="https://docs.joinmastodon.org/entities/V1_Instance/">Mastodon API documentation: entities/V1_Instance</a>
+     * Obtain general information about the server.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#v2">Mastodon API documentation: methods/instance/#v2</a>
      */
     fun getInstance(): MastodonRequest<Instance> {
         return client.getMastodonRequest(
-            endpoint = "api/v1/instance",
+            endpoint = instanceEndpointV2,
+            method = MastodonClient.Method.GET
+        )
+    }
+
+    /**
+     * Obtain general information about the server.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#v1">Mastodon API documentation: methods/instance/#v1</a>
+     */
+    @Deprecated(
+        message = "Deprecated since 4.0.0. This method is just kept for backward compatibility.",
+        replaceWith = ReplaceWith("getInstance()"),
+        level = DeprecationLevel.WARNING
+    )
+    fun getInstanceV1(): MastodonRequest<InstanceV1> {
+        return client.getMastodonRequest(
+            endpoint = instanceEndpointV1,
+            method = MastodonClient.Method.GET
+        )
+    }
+
+    /**
+     * Get the list of connected domains: Domains that this instance is aware of.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#peers">Mastodon API documentation: methods/instance/#peers</a>
+     */
+    fun getPeers(): MastodonRequest<List<String>> {
+        return client.getMastodonRequestForList(
+            endpoint = "$instanceEndpointV1/peers",
+            method = MastodonClient.Method.GET
+        )
+    }
+
+    /**
+     * Instance activity over the last 3 months, binned weekly.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#activity">Mastodon API documentation: methods/instance/#activity</a>
+     */
+    fun getActivity(): MastodonRequest<List<InstanceActivity>> {
+        return client.getMastodonRequestForList<InstanceActivity>(
+            endpoint = "$instanceEndpointV1/activity",
+            method = MastodonClient.Method.GET
+        )
+    }
+
+    /**
+     * Rules that the users of this service should follow.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#rules">Mastodon API documentation: methods/instance/#rules</a>
+     */
+    fun getRules(): MastodonRequest<List<Rule>> {
+        return client.getMastodonRequestForList<Rule>(
+            endpoint = "$instanceEndpointV1/rules",
+            method = MastodonClient.Method.GET
+        )
+    }
+
+    /**
+     * View moderated servers
+     * Obtain a list of domains that have been blocked.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#domain_blocks">Mastodon API documentation: methods/instance/#domain_blocks</a>
+     */
+    fun getBlockedDomains(): MastodonRequest<List<DomainBlock>> {
+        return client.getMastodonRequestForList<DomainBlock>(
+            endpoint = "$instanceEndpointV1/domain_blocks",
+            method = MastodonClient.Method.GET
+        )
+    }
+
+    /**
+     * Obtain an extended description of this server.
+     * @see <a href="https://docs.joinmastodon.org/methods/instance/#extended_description">
+     *     Mastodon API documentation: methods/instance/#extended_description</a>
+     */
+    fun getExtendedDescription(): MastodonRequest<ExtendedDescription> {
+        return client.getMastodonRequest(
+            endpoint = "$instanceEndpointV1/extended_description",
             method = MastodonClient.Method.GET
         )
     }
