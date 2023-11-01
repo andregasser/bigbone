6430bc4aac91ca9e69d7ee1dd1d67fab2dae9ad1
gioxi <desiderio.gio@yahoo.com>
2023-11-01 22:47:49 +0100

    fix tests

--- bigbone/src/test/kotlin/social/bigbone/api/method/ReportMethodsTest.kt
+++ bigbone/src/test/kotlin/social/bigbone/api/method/ReportMethodsTest.kt
@@ -1,20 +1,12 @@
 package social.bigbone.api.method
 
 import org.amshove.kluent.`should be equal to`
-import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldContainIgnoringCase
-import org.amshove.kluent.shouldNotBeNull
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import social.bigbone.PrecisionDateTime.InvalidPrecisionDateTime.Unavailable
-import social.bigbone.PrecisionDateTime.ValidPrecisionDateTime.ExactTime
-import social.bigbone.PrecisionDateTime.ValidPrecisionDateTime.StartOfDay
-import social.bigbone.api.entity.Report.ReportType
+import social.bigbone.api.entity.Report.ReportCategory
 import social.bigbone.api.exception.BigBoneRequestException
 import social.bigbone.testtool.MockClient
-import java.time.Instant
-import java.time.LocalDate
-import java.time.ZoneOffset
 
 class ReportMethodsTest {
 
@@ -22,8 +14,8 @@ class ReportMethodsTest {
     fun reportFile() {
         val client = MockClient.mock("reports.json")
         val fileReportMethod = ReportMethods(client)
-        val result = fileReportMethod.fileReport(accountId = "testId", category = ReportType.SPAM, comment = "spam").execute()
-        result.category `should be equal to` ReportType.SPAM.type
+        val result = fileReportMethod.fileReport(accountId = "testId", category = ReportCategory.SPAM, comment = "spam").execute()
+        result.category `should be equal to` ReportCategory.SPAM.name.lowercase()
         result.comment shouldContainIgnoringCase "spam"
         result.id `should be equal to` "testId"
     }
