0062672ad5092728b0d3c2ae61dbd0db5b238b4f
gioxi <desiderio.gio@yahoo.com>
2023-11-01 20:32:32 +0100

    add method docs within RxPushNotificationMethods

--- bigbone-rx/src/main/kotlin/social/bigbone/rx/RxPushNotificationMethods.kt
+++ bigbone-rx/src/main/kotlin/social/bigbone/rx/RxPushNotificationMethods.kt
@@ -15,6 +15,25 @@ class RxPushNotificationMethods(client: MastodonClient) {
 
     private val pushNotificationMethods = PushNotificationMethods(client)
 
+    /**
+     * Add a Web Push API subscription to receive notifications.
+     * Each access token can have one push subscription. If you create a new subscription, the old subscription is deleted.
+     * @param endpoint The endpoint URL that is called when a notification event occurs.
+     * @param userPublicKey  User agent public key. Base64 encoded string of a public key from a ECDH keypair using the prime256v1 curve.
+     * @param userAuthSecret Auth secret, Base64 encoded string of 16 bytes of random data.
+     * @param mention Receive mention notifications?
+     * @param status Receive new subscribed account notifications?
+     * @param reblog Receive reblog notifications?
+     * @param follow Receive follow notifications?
+     * @param followRequest Receive follow request notifications?
+     * @param favourite Receive favourite notifications?
+     * @param poll Receive poll notifications?
+     * @param update Receive status edited notifications?
+     * @param adminSignUp Receive new user signup notifications? Defaults to false. Must have a role with the appropriate permissions.
+     * @param adminReport Receive new report notifications? Defaults to false. Must have a role with the appropriate permissions.
+     * @param policy Specify which to receive push notifications from.
+     * @see <a href="https://docs.joinmastodon.org/methods/push/#create">Mastodon API documentation: methods/push/#create</a>
+     */
     @JvmOverloads
     fun subscribePushNotification(
         endpoint: String,
@@ -51,6 +70,22 @@ class RxPushNotificationMethods(client: MastodonClient) {
             ).execute()
         }
 
+    /**
+     * Updates the current push subscription. Only the data part can be updated.
+     * To change fundamentals, a new subscription must be created instead.
+     * @param mention Receive mention notifications?
+     * @param status Receive new subscribed account notifications?
+     * @param reblog Receive reblog notifications?
+     * @param follow Receive follow notifications?
+     * @param followRequest Receive follow request notifications?
+     * @param favourite Receive favourite notifications?
+     * @param poll Receive poll notifications?
+     * @param update Receive status edited notifications?
+     * @param adminSignUp Receive new user signup notifications? Defaults to false. Must have a role with the appropriate permissions.
+     * @param adminReport Receive new report notifications? Defaults to false. Must have a role with the appropriate permissions.
+     * @param policy Specify which to receive push notifications from.
+     * @see <a href="https://docs.joinmastodon.org/methods/push/#update">Mastodon API documentation: methods/push/#update</a>
+     */
     @JvmOverloads
     fun updatePushSubscription(
         mention: Boolean? = false,
@@ -81,7 +116,15 @@ class RxPushNotificationMethods(client: MastodonClient) {
             ).execute()
         }
 
+    /**
+     * View the PushSubscription currently associated with this access token.
+     * @see <a href="https://docs.joinmastodon.org/methods/push/#get">Mastodon API documentation: methods/push/#get</a>
+     */
     fun getPushNotification(): Single<WebPushSubscription> = Single.fromCallable { pushNotificationMethods.getPushNotification().execute() }
 
+    /**
+     * Removes the current Web Push API subscription.
+     * @see <a href="https://docs.joinmastodon.org/methods/push/#delete">Mastodon API documentation: methods/push/#delete</a>
+     */
     fun removePushSubscription(): Completable = Completable.fromAction { pushNotificationMethods.removePushSubscription() }
 }
