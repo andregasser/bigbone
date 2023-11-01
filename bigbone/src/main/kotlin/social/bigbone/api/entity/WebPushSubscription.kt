1f0bc198372c0551579860195040b9e2d8c7c7fa
gioxi <desiderio.gio@yahoo.com>
2023-11-01 20:30:45 +0100

    update WebPushSubscription with default values of attributes

--- bigbone/src/main/kotlin/social/bigbone/api/entity/WebPushSubscription.kt
+++ bigbone/src/main/kotlin/social/bigbone/api/entity/WebPushSubscription.kt
@@ -14,19 +14,19 @@ data class WebPushSubscription(
      * The ID of the Web Push subscription in the database.
      */
     @SerialName("id")
-    val id: String,
+    val id: String = "",
 
     /**
      * Where push alerts will be sent to.
      */
     @SerialName("endpoint")
-    val endpoint: String,
+    val endpoint: String = "",
 
     /**
      * The streaming server’s VAPID key.
      */
     @SerialName("server_key")
-    val serverKey: String,
+    val serverKey: String = "",
 
     /**
      * Which alerts should be delivered to the endpoint.
