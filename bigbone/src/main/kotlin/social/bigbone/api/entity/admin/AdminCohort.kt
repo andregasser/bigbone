fc95f95c650896e60a6e83fc48a68c856231759f
Patrick Geselbracht <code@patrick-geselbracht.eu>
2023-11-06 14:03:09 +0100

    Add admin/retention methods (#334)
    * Allow parsing of time strings with +00:00 as well as Z
    
    * Add admin/retention methods

---
+++ bigbone/src/main/kotlin/social/bigbone/api/entity/admin/AdminCohort.kt
@@ -0,0 +1,81 @@
+package social.bigbone.api.entity.admin
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import social.bigbone.DateTimeSerializer
+import social.bigbone.PrecisionDateTime
+
+/**
+ * Represents a retention metric.
+ * @see <a href="https://docs.joinmastodon.org/entities/Admin_Cohort/">Mastodon documentation Admin::Cohort</a>
+ *
+ */
+@Serializable
+data class AdminCohort(
+
+    /**
+     * The timestamp for the start of the period, at midnight.
+     */
+    @SerialName("period")
+    @Serializable(with = DateTimeSerializer::class)
+    val period: PrecisionDateTime = PrecisionDateTime.InvalidPrecisionDateTime.Unavailable,
+
+    /**
+     * The size of the bucket for the returned data.
+     */
+    @SerialName("frequency")
+    val frequency: FrequencyOneOf? = null,
+
+    /**
+     * Retention data for users who registered during the given period.
+     */
+    @SerialName("data")
+    val data: List<CohortData>? = null
+) {
+    /**
+     * The size of the bucket for the returned data.
+     */
+    @Serializable
+    enum class FrequencyOneOf {
+        /**
+         * Daily buckets.
+         */
+        @SerialName("day")
+        DAY,
+
+        /**
+         * Monthly buckets.
+         */
+        @SerialName("month")
+        MONTH;
+
+        @OptIn(ExperimentalSerializationApi::class)
+        val apiName: String get() = serializer().descriptor.getElementName(ordinal)
+    }
+
+    /**
+     * Retention data for users who registered during the given period.
+     */
+    @Serializable
+    data class CohortData(
+        /**
+         * The timestamp for the start of the bucket, at midnight.
+         */
+        @SerialName("date")
+        @Serializable(with = DateTimeSerializer::class)
+        val date: PrecisionDateTime = PrecisionDateTime.InvalidPrecisionDateTime.Unavailable,
+
+        /**
+         * The percentage rate of users who registered in the specified period and were active for the given date bucket.
+         */
+        @SerialName("rate")
+        val rate: Float? = null,
+
+        /**
+         * How many users registered in the specified period and were active for the given date bucket.
+         */
+        @SerialName("value")
+        val value: String? = null
+    )
+}
