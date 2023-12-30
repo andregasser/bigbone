c9b41a888e1ffab03ab90a11899453075c884c3d
Patrick Geselbracht <code@patrick-geselbracht.eu>
2023-12-30 23:35:59 +0100

    Add default arguments to ease testing

--- bigbone/src/main/kotlin/social/bigbone/api/entity/admin/AdminReport.kt
+++ bigbone/src/main/kotlin/social/bigbone/api/entity/admin/AdminReport.kt
@@ -20,13 +20,13 @@ data class AdminReport(
      * String cast from an integer, but not guaranteed to be a number.
      */
     @SerialName("id")
-    val id: String,
+    val id: String = "",
 
     /**
      * Whether an action was taken to resolve this report.
      */
     @SerialName("action_taken")
-    val actionTaken: Boolean,
+    val actionTaken: Boolean = false,
 
     /**
      * When an action was taken, if this report is currently unresolved.
@@ -40,7 +40,7 @@ data class AdminReport(
      * @see ReportCategory
      */
     @SerialName("category")
-    val category: ReportCategory,
+    val category: ReportCategory = ReportCategory.OTHER,
 
     /**
      * An optional reason for reporting.
@@ -52,7 +52,7 @@ data class AdminReport(
      * Whether the report was forwarded to a remote instance.
      */
     @SerialName("forwarded")
-    val forwarded: Boolean,
+    val forwarded: Boolean = false,
 
     /**
      * The time the report was filed.
@@ -72,37 +72,37 @@ data class AdminReport(
      * The account which filed the report.
      */
     @SerialName("account")
-    val reportCreator: AdminAccount,
+    val reportCreator: AdminAccount = AdminAccount(),
 
     /**
      * The account being reported.
      */
     @SerialName("target_account")
-    val reportedAccount: AdminAccount,
+    val reportedAccount: AdminAccount = AdminAccount(),
 
     /**
      * The account of the moderator assigned to this report.
      * May be null if no moderator was assigned.
      */
     @SerialName("assigned_account")
-    val assignedModerator: AdminAccount?,
+    val assignedModerator: AdminAccount? = null,
 
     /**
      * The account of the moderator who handled the report.
      * May be null if no action has been taken on this report by any moderator yet.
      */
     @SerialName("action_taken_by_account")
-    val handledBy: AdminAccount?,
+    val handledBy: AdminAccount? = null,
 
     /**
      * Statuses attached to the report, for context.
      */
     @SerialName("statuses")
-    val attachedStatuses: List<Status>,
+    val attachedStatuses: List<Status> = emptyList(),
 
     /**
      * Rules attached to the report, for context.
      */
     @SerialName("rules")
-    val attachedRules: List<Rule>
+    val attachedRules: List<Rule> = emptyList()
 )
